510708df1ef91d1e631cdccb91073b1ff37ccd57
muthukannappan <21cs039@acetcbe.edu.in>
2025-09-01 19:54:08 +0530

    Modified

--- Max_array.java
+++ Max_array.java
@@ -1,3 +1,12 @@
 public class Max_array {
-    
+    public static void main(String[] args) {
+		int[] arr= {10,20,4,45,99,6};
+		int max=arr[0];
+		 for (int i=1;i<arr.length;i++) {
+			 if(arr[i]>max) {
+				 max=arr[i];
+			 }
+		 }
+		 System.out.println(max);
+    }
 }
